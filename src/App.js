daf7e9a6a1899d09bd0477056d15eff9e00f2a5e
Marta Coelho <freelancer.martacoelho@gmail.com>
2022-10-15 18:23:36 +0100

    Form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a
+              href="https://github.com/MartaCoelho750/dictionary-project"
+              rel=" noopener noreferrer"
+              target="_blank"
+            >
+              Open Source Code By
+            </a>{" "}
+            Marta Coelho for SheCodes
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
