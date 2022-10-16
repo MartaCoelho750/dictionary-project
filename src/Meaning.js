30c35ab3c9bf67c830b6946c43732f8f7329a62a
Marta Coelho <freelancer.martacoelho@gmail.com>
2022-10-16 16:10:28 +0100

    updated meanings

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Meaning(props) {
+  console.log({ props });
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partsOfSpeech}</h3>
-      {props.meaning.defenitions.map(function (definition, index) {
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
